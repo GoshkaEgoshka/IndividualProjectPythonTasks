d3f283e3163d0cff8e6840a140e13974ead6b43e
Yehor Koval'ov <bingobingo1997@gmail.com>
2019-02-28 10:25:57 +0200

    Was created input.py module
    This module is used for data entry and is imported into Python tasks. Contains 4 classes. One class  abstract, the SetInputValue class, requests data, the InputToInt class maps the entered data to an integer value, and the InputCache class caches the entered values.

---
+++ input.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Input(ABC):
+    @abstractmethod
+    def value(self) -> Any:
+        pass
+
+
+class SetInputValue(Input):
+    def __init__(self, input_text: str):
+        self._text = input_text
+
+    def value(self) -> Any:
+        return input(self._text)
+
+
+class InputToInt(Input):
+    def __init__(self, some_value: Any):
+        self._value = some_value
+
+    def value(self) -> Any:
+        return int(self._value.value())
+
+
+class InputCache(Input):
+    def __init__(self, ask: Input):
+        self._ask = ask
+        self._data = []
+
+    def value(self) -> Any:
+        if not self._data:
+            self._data.append(self._ask.value())
+        return self._data[0]
+
+
